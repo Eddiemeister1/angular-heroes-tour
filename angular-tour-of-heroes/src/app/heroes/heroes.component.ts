f98463c16389c95025850102a4621cf2ad8f72c8
Eduardo Dotel <42874513+Eddiemeister1@users.noreply.github.com>
2021-06-07 17:16:18 +0000

    Created a new component for hero details and a hero service

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -7,11 +7,7 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-
+  selectedHero?: Hero;
   heroes = HEROES;
   constructor() { }
 
@@ -19,4 +15,7 @@ export class HeroesComponent implements OnInit {
 
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
