2b1ad3f4f446f1f89b3cb5c2d0c7aece33dddd7d
Eduardo Dotel <42874513+Eddiemeister1@users.noreply.github.com>
2021-06-07 15:25:46 +0000

    Added the hero list and details

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from "./interfaces/hero";
+
+export const HEROES: Hero[] = [
+  { id: 2, name: 'Spider-Man' },
+  { id: 3, name: 'Batman' },
+  { id: 4, name: 'Superman' },
+  { id: 5, name: 'Wonder Woman' },
+  { id: 6, name: 'Captain America' },
+  { id: 7, name: 'Iron Man' },
+  { id: 8, name: 'Ant Man' },
+  { id: 9, name: 'Cyborg' },
+  { id: 10, name: 'Robin' }
+];
